d36ce2e423f28606366714db7de4664619e5ae23
Kellis <vikiclashonyt@gmail.com>
2020-05-23 23:59:25 +0200

    Update list.php

--- list.php
+++ list.php
@@ -4,14 +4,17 @@ if (!empty($_GET['number'])) {
     
     $number = htmlspecialchars($_GET['number']);
 
-    $reqnumber = $bdd->prepare("SELECT * FROM scam_incs WHERE numbers LIKE CONCAT(?, '%')");
+    $reqnumber = $bdd->prepare("SELECT * FROM scam_incs WHERE numbers OR website OR name OR email LIKE CONCAT('%', ?, '%')");
     $reqnumber->execute(array($number));
     $numberexist = $reqnumber->rowCount();
 
     if ($numberexist != 0) {
-        $number = $reqnumber->fetch();
-
-        echo $number['name'].'*'.$number['website'].'*'.$number['numbers'].'*'.$number['email'].'*'.$number['other_name'];
+        if ($numberexist > 1) {
+            echo "Error2*".$numberexist;
+        } else {
+            $number = $reqnumber->fetch();
+            echo $number['name'].'*'.$number['website'].'*'.$number['numbers'].'*'.$number['email'].'*'.$number['other_name'];
+        }
     } else {
         echo "Error";
     }
